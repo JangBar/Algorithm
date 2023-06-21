198890307257d722a5dd770a4407da0f2a2b2980
O HYEOK <atta369@naver.com>
2023-06-21 20:32:07 +0900

    [Bronze III] Title: 곱셈, Time: 144 ms, Memory: 14660 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -9,16 +9,18 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int a, c, d, e;
+		String str1,str2;
 		String[] b;
-		String str1 = br.readLine();
-		String str2 = br.readLine();
+		
+		str1 = br.readLine(); // 첫번째 입력 "472"
+		str2 = br.readLine(); // 두번째 입력 "385"
 
-		a = Integer.parseInt(str1);
-		b = str2.split("");
-		c = b.length - 1;
+		a = Integer.parseInt(str1); // 첫번째 입력받은수를 숫자로변환 472
+		b = str2.split("");         // 두번째 입력받은수를 하나하나 나눠서 배열에 담는다 [3,8,5]
+		c = b.length - 1;           // 배열의 길이를 구해서 -1을 해준다 
 		d = 0;
 		e = 10;
-
+		
 		for (int i = c; i >= 0; i--) {
 			bw.write((a * Integer.parseInt(b[i])) + "\n");
 			if (i == c) {
