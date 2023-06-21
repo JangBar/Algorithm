8420bd6cf081a2ccb1a2652e836d913497b6c158
O HYEOK <atta369@naver.com>
2023-06-21 21:42:40 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13872 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,8 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
+		
 		System.out.println("\\    /\\");
 		System.out.println(" )  ( ')");
 		System.out.println("(  /  )");
-		System.out.println(" \\(__)|");
+		System.out.println(" \\(__)|");	
 	}
 }
