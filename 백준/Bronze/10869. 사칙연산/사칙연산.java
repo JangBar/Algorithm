1088993d99d35dcf173b2b7c04f256d8c2b1e382
O HYEOK <atta369@naver.com>
2023-06-21 17:17:45 +0900

    [Bronze V] Title: 사칙연산, Time: 144 ms, Memory: 15900 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int a,b;
+        String[] strs = br.readLine().split(" ");
+
+
+        a = Integer.parseInt(strs[0]);
+        b = Integer.parseInt(strs[1]);
+        
+        bw.write((a + b)+" "+"\n");
+        bw.write((a - b)+" "+"\n");
+        bw.write((a * b)+" "+"\n");
+        bw.write((a / b)+" "+"\n");
+        bw.write((a % b)+" ");
+        bw.flush();
+        bw.close();
+    }
+}
