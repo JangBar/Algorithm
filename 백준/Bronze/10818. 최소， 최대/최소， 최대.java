fa76881ff2b59b5543ca9425a0db9456a4d87518
O HYEOK <atta369@naver.com>
2023-06-23 14:51:57 +0900

    [Bronze III] Title: 최소, 최대, Time: 528 ms, Memory: 88316 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -6,33 +6,33 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 
-        int num1 = Integer.parseInt(br.readLine());
+		int num1 = Integer.parseInt(br.readLine());
 
-        int num3 = Integer.MIN_VALUE;
-        int num4 = Integer.MAX_VALUE;
-        st = new StringTokenizer(br.readLine(), " ");
+		int maxValue = Integer.MIN_VALUE;
+		int minValue = Integer.MAX_VALUE;
+		st = new StringTokenizer(br.readLine(), " ");
 
-        for (int i = 1; i <= num1; i++) {
-            int num2 = Integer.parseInt(st.nextToken());
+		for (int i = 1; i <= num1; i++) {
+			int num2 = Integer.parseInt(st.nextToken());
 
-            if (num2 < num4) {
-                num4 = num2;
-            }
+			if (num2 < minValue) {
+				minValue = num2;
+			}
 
-            if (num2 > num3) {
-                num3 = num2;
-            }
-        }
+			if (num2 > maxValue) {
+				maxValue = num2;
+			}
+		}
 
-        bw.write(String.valueOf(num4 + " " + num3));
+		bw.write(String.valueOf(minValue + " " + maxValue));
 
-        br.close();
-        bw.flush();
-        bw.close();
-    }
+		br.close();
+		bw.flush();
+		bw.close();
+	}
 }
