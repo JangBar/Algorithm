618f153227d32def7ffe5ff2a9679c6ca2b8cf8e
O HYEOK <atta369@naver.com>
2023-06-27 23:20:59 +0900

    [Bronze V] Title: A+B - 2, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int n1 = Integer.parseInt(br.readLine());
+		int n2 = Integer.parseInt(br.readLine());
+
+		bw.write(String.valueOf(n1 + n2));
+		bw.newLine();
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
