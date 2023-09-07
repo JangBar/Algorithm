2ca405078a75687506755e789aef68eb826046f8
O HYEOK <atta369@naver.com>
2023-09-07 10:51:04 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.cpp
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -3,12 +3,10 @@
 using namespace std;
 int solution(int n)
 {
-     int answer = 0;
-     string num = to_string(n);
-     for (char result : num) {
-     // 문자를 정수로 변환하려면 문자 - '0'을 사용합니다.
-     int digit = result - '0';
-     answer += digit;
- }
+    int answer = 0;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
     return answer;
 }
